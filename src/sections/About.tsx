e860de714763911c79d93b07faff99c57986d1d9
HemanthBear99 <appalahemanth413@gmail.com>
2025-03-05 08:34:12 +0530

    About us

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,3 +1,62 @@
+import JavaScriptIcon from '@/assets/icons/square-js.svg';
+import StarIcon from '@/assets/icons/star.svg';
+import bookImage from '@/assets/images/book-cover.png';
+import { Card } from '@/components/Card';
+import { SectionHeader } from '@/components/SectionHeader';
+import { TechIcon } from '@/components/TechIcon';
+import Image from 'next/image';
+
+const toolboxItems = [
+  {
+    title: 'JavaScript',
+    iconType: JavaScriptIcon,
+  },
+];
+
 export const AboutSection = () => {
-  return <div>About Section</div>;
+  return (
+    <div className="pb-96">
+      <SectionHeader
+        eyebrow="About Me"
+        title="A Glimpse Into My World"
+        description=" More about who I am and what makes me special. This should be changed in About.tsx file"
+      />
+      <div>
+        <Card>
+          <div>
+            <StarIcon />
+            <h3>My Reads</h3>
+            <p>Explore the books shaping my perspectives</p>
+          </div>
+          <Image src={bookImage} alt="Book cover" />
+        </Card>
+        <Card>
+          <div>
+            <StarIcon />
+            <h3>My Toolbox</h3>
+            <p>
+              Explore the technologies and tools I use to craft exceptional
+              digital experiences
+            </p>
+          </div>
+          <div>
+            {toolboxItems.map((item) => (
+              <div key={item.title}>
+                <TechIcon component={item.iconType} />
+                <span>{item.title}</span>
+              </div>
+            ))}
+          </div>
+        </Card>
+        <Card>
+          <div>
+            <StarIcon />
+            <h3>Beyond the Code</h3>
+            <p>Explore my interests and hobbies that make me a well-rounded</p>
+          </div>
+        </Card>
+        <Card></Card>
+      </div>
+    </div>
+  );
 };
--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -43,7 +43,7 @@ const testimonials = [
 
 export const TestimonialsSection = () => {
   return (
-    <div className="py-16">
+    <div className="py-16 lg:py-24">
       <div className="container">
         <SectionHeader
           eyebrow="Happy Clients"
@@ -51,12 +51,15 @@ export const TestimonialsSection = () => {
           description="Dont just take my word for it  See what my clents have to say about my
         work this shuld be changed after in testimonials.tsx"
         />
-        <div className="mt-16 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)] [-webkit-mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+        <div className="mt-16 lg:mt-24 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)] [-webkit-mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
           <div className=" flex gap-8 flex-none">
             {testimonials.map((testimonial) => (
-              <Card key={testimonial.name} className="max-w-xs">
+              <Card
+                key={testimonial.name}
+                className="max-w-xs md:max-w-md md:p-8"
+              >
                 <div className="flex gap-4 items-center">
-                  <div className="size-14 bg-gray-700 inline-flex rounded-full items-center justify-center">
+                  <div className="size-14 bg-gray-700 inline-flex rounded-full items-center justify-center flex-shrink-0">
                     <Image
                       src={testimonial.avatar}
                       alt={testimonial.name}
@@ -70,7 +73,9 @@ export const TestimonialsSection = () => {
                     </div>
                   </div>
                 </div>
-                <p className="mt-4 text-sm">{testimonial.text}</p>
+                <p className="mt-4 md:mt-6 text-sm md:text-base">
+                  {testimonial.text}
+                </p>
               </Card>
             ))}
           </div>
